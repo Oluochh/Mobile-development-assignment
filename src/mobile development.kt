7491600ba303822d27dc17cc0e1ca0f08a68aa5d
oluochh <gillyoluochh@gmail.com>
2021-02-21 11:09:18 +0300

    first commit

---
+++ src/mobile development.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val name:String=("janeNyambura")
+            println(name)
+
+    var phoneNumber:String="0792888497"
+    println(phoneNumber)
+   var citizen:String="ugandan"
+    println(citizen)
+    var age: Byte=20
+    println(age)
+    var weight:Float=58.654.toFloat()
+    println(weight)
+}
