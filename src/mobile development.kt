4637652f10e6c0ec2548fef4dd5db6c055ea8d03
oluochh <gillyoluochh@gmail.com>
2021-02-28 17:30:20 +0300

    my first trial

--- src/mobile development.kt
+++ src/mobile development.kt
@@ -1,13 +1,29 @@
 fun main() {
-    val name:String=("janeNyambura")
-            println(name)
+println("hello gilly")
+    division()
+    add()
+    println(true)
+}
+fun gilly() {
+    println("hello gilly")
+}
+fun division() {
+    var a=13
+    var b=5
+    var division=a/b
+    println(division)
+}
+fun add() {
+   var a=6
+    var b=3
+    var c=9
+    var d=2
+    var sum=a+b+c+d
+    println(sum)
+}
+fun me() {
+ var me="i like eating"
+    println(true)
 
-    var phoneNumber:String="0792888497"
-    println(phoneNumber)
-   var citizen:String="ugandan"
-    println(citizen)
-    var age: Byte=20
-    println(age)
-    var weight:Float=58.654.toFloat()
-    println(weight)
+
 }
